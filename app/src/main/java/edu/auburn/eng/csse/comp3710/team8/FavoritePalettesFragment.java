3daff621268d305f2a9ee639c71ccfd6b3b8f799
zealousduck <stewartpatrick12@gmail.com>
2015-04-08 15:23:14 -0500

    Initial Cleanup Test

--- app/src/main/java/edu/auburn/eng/csse/comp3710/team8/FavoritePalettesFragment.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/team8/FavoritePalettesFragment.java
@@ -11,6 +11,7 @@ public class FavoritePalettesFragment extends Fragment {
 
     public interface FavoritePalettesListener {
         public void onFragmentInteraction();
+        public void foo();
     }
 
     private FavoritePalettesListener mListener;
