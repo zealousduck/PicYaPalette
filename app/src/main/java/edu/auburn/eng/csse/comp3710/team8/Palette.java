5176cdf6b835a44757aded6f4d6c5d99e6585e3c
zealousduck <stewartpatrick12@gmail.com>
2015-04-22 13:02:21 -0500

    Created the Shell of the app

---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/team8/Palette.java
@@ -0,0 +1,45 @@
+package edu.auburn.eng.csse.comp3710.team8;
+
+import android.graphics.Color;
+
+/**
+ * Created by patrickstewart on 4/22/15.
+ */
+public abstract class Palette {
+
+    protected static int COUNTER = 0; // Used for numbering palettes for default names
+    protected static final String DEFAULT_NAME = "Palette #";
+
+    protected String name;
+    protected Color primary, secondary, tertiary;
+    // List<Color> colors;
+
+    public Palette(Color base) {
+        this.primary = base;
+        this.name = DEFAULT_NAME + COUNTER++;
+        // Generate secondary and tertiary colors!
+    }
+
+    public Palette(Color primary, Color secondary) {
+        this.primary = primary;
+        this.secondary = secondary;
+        this.name = DEFAULT_NAME + COUNTER++;
+        // Generate tertiary colors!
+    }
+
+    public Palette(Color primary, Color secondary, Color tertiary) {
+        this.primary = primary;
+        this.secondary = secondary;
+        this.tertiary = tertiary;
+        this.name = DEFAULT_NAME + COUNTER++;
+    }
+
+    public void setName(String nameIn) {
+        this.name = nameIn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
--- app/src/main/java/edu/auburn/eng/csse/comp3710/team8/PicYaPaletteActivity.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/team8/PicYaPaletteActivity.java
@@ -1,52 +1,30 @@
 package edu.auburn.eng.csse.comp3710.team8;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
 
-
+/* USED AS A SPLASH SCREEN!
+ */
 public class PicYaPaletteActivity extends Activity {
+
+    private static int SPLASH_TIMER = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pic_ya_palette);
-        int one = 1;
+
+        // Time out after SPLASH_TIMER milliseconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(PicYaPaletteActivity.this, ImageChooserActivity.class);
+                startActivity(i);
+            }
+        }, SPLASH_TIMER);
+
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_pic_ya_palette, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
