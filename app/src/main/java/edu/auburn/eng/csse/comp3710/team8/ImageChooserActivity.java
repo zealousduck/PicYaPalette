6cffdbd5b8143acc9b21c0723b200716015bcdbd
zealousduck <stewartpatrick12@gmail.com>
2015-04-22 19:12:42 -0500

    Getting shell of code

--- app/src/main/java/edu/auburn/eng/csse/comp3710/team8/ImageChooserActivity.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/team8/ImageChooserActivity.java
@@ -1,17 +1,49 @@
 package edu.auburn.eng.csse.comp3710.team8;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
 
 public class ImageChooserActivity extends Activity {
+
+    private Button mTakePic;
+    private Button mChoosePic;
+    private Button mFavorites;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_chooser);
+
+        // Hook up Buttons
+        mTakePic =      (Button)findViewById(R.id.button_take);
+        mTakePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle taking a picture!
+            }
+        });
+        mChoosePic =    (Button)findViewById(R.id.button_choose);
+        mChoosePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle choosing a picture!
+            }
+        });
+        mFavorites =    (Button)findViewById(R.id.button_favorites);
+        mFavorites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch Favorites activity!
+                Intent i = new Intent(ImageChooserActivity.this, FavoritePalettesActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
 
