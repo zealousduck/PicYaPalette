0382290bc5afa088516d7bf18631f58c87899fa9
zealousduck <stewartpatrick12@gmail.com>
2015-04-24 13:34:10 -0500

    Made some comments to define a rough interface for interacting with our saved palettes file/database

--- app/src/main/java/edu/auburn/eng/csse/comp3710/team8/PaletteStorageHelper.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/team8/PaletteStorageHelper.java
@@ -1,24 +1,54 @@
 package edu.auburn.eng.csse.comp3710.team8;
 
-import java.util.List;
-
 /**
- * Created by Patrick on 4/22/2015.
+ * NOTES: Okay so I really don't have any good suggestions for exactly how this class will
+ * be implemented. I'm starting with static methods so that we can access the data
+ * without needing to instantiate the Helper -- this COULD be a terrible way to do it though.
+ *
  */
-public  class PaletteStorageHelper {
+public class PaletteStorageHelper {
 
-    public static void save(Palette p) {
+    /* I'll definitely need this method, as this'll be called by PaletteDetailsActivity
+     * when the "Save Palette to Favorites" button is pressed.
+     */
+    public static boolean save(Palette p) {
         // Add palette to favorites
+
+        return true; // Return true for success
     }
 
-    public static void unsave(Palette p) {
+    /* Definitely needed so that users can remove palettes from their favorites. I'm also
+     * considering making this callable as a sort of anti-button within PaletteDetailsActivity
+     * i.e. If the palette hasn't been saved, Save it. If it HAS been saved, remove it.
+     *      (See isSaved() comments)
+     */
+    public static boolean remove(Palette p) {
         // Remove palette from favorites
+
+        return true; // Return true for success
     }
 
+    /* While not strictly necessary, this could be useful for checking if we're trying to save
+     * an identical palette / seeing if a palette has already been saved.
+     */
+    public static boolean isSaved(Palette p) {
+        // Check if palette has already been inserted into favorites
 
+        return true; // Return true for success
 
+    }
+
+    /* TOP-TIER IMPORTANCE: PaletteAdapter REQUIRES a Palette[] in order to function,
+     * and I'll be giving a PaletteAdapter to FavoritePalettesActivity to populate
+     * the ListView.
+     */
     public static Palette[] getAllPalettes() {
         return null;
     }
+
+    /* Again, this is just a rough outline for this interface. I've given my input as a sort
+     * of hypothetical, but if you end up needing to make some changes it won't be a big deal.
+     * However you can make it work is good for me. -- Patrick
+     */
 
 }
