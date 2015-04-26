e62745bce0fe8360a56c3cb37ea5521600a45fc9
Sam Bartlett <sam@sasbartlett.com>
2015-04-25 21:40:19 -0500

    Database Class creation and modification. Set up the two classes needed for saving favorite palettes. Should be ready to go.

---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/team8/DBManager.java
@@ -0,0 +1,43 @@
+package edu.auburn.eng.csse.comp3710.team8;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Sam on 4/25/15.
+ */
+public class DBManager extends SQLiteOpenHelper{
+
+    private static final String DATABASE_NAME = "YaSavedPalettes.db";
+    public static final String TABLE_PALETTES = "palettes";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_COLORS = "colors";
+    public static final String COLUMN_NAME = "name";
+    private static final int DATABASE_VERSION = 1;
+
+    // Database creation sql statement
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_PALETTES + "(" + COLUMN_ID
+            + " integer primary key autoincrement, " + COLUMN_NAME
+            + " text not null, " + COLUMN_COLORS + " text not null);";
+
+    public DBManager(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w("DBManager",
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PALETTES);
+        onCreate(db);
+
+    }
+}
--- app/src/main/java/edu/auburn/eng/csse/comp3710/team8/PaletteStorageHelper.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/team8/PaletteStorageHelper.java
@@ -1,40 +1,78 @@
 package edu.auburn.eng.csse.comp3710.team8;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.SQLException;
+import android.content.ContentValues;
 
 /**
  * NOTES: Okay so I really don't have any good suggestions for exactly how this class will
  * be implemented. I'm starting with static methods so that we can access the data
  * without needing to instantiate the Helper -- this COULD be a terrible way to do it though.
  *
+ * It is a kind of bad way to do it - simply because it's hard to initialize the db - but I will
+ * create simple methods to be called to set up the databases on initialization.
  */
 public class PaletteStorageHelper {
 
-    /* I'll definitely need this method, as this'll be called by PaletteDetailsActivity
-     * when the "Save Palette to Favorites" button is pressed.
-     */
-    public static boolean save(Palette p) {
-        // Add palette to favorites
+    private SQLiteDatabase database;
+    private DBManager dBManager;
+    private String[] allColumns = { DBManager.COLUMN_ID, DBManager.COLUMN_NAME, DBManager.COLUMN_COLORS};
+    public static final int SUCCESS = 0;
+    public static final int DUPLICATE = 1;
+    public static final int FAILURE = 2;
 
-        return true; // Return true for success
+    public PaletteStorageHelper(Context context) {
+        dBManager = new DBManager(context);
     }
 
-    /* Definitely needed so that users can remove palettes from their favorites. I'm also
-     * considering making this callable as a sort of anti-button within PaletteDetailsActivity
-     * i.e. If the palette hasn't been saved, Save it. If it HAS been saved, remove it.
-     *      (See isSaved() comments)
-     */
-    public static boolean remove(Palette p) {
-        // Remove palette from favorites
+    public void open() throws SQLException {
+        database = dBManager.getWritableDatabase();
+    }
 
+
+    public void close() {
+        dBManager.close();
+    }
+
+    /*
+    *
+    * Saves the palette and returns a code
+    *
+     */
+    public int save(Palette p) {
+        ContentValues values = new ContentValues();
+        values.put("name", p.getName());
+        String temp = colorsToString(p);
+        if(!isSaved(p)) {
+            values.put("colors", temp);
+            Long insertId = database.insert(DBManager.TABLE_PALETTES, null, values);
+            if (insertId != null) return SUCCESS;
+            else return FAILURE;
+        }
+        return DUPLICATE;
+    }
+
+    /*
+     *      Removes the palette from the databse
+     */
+    public boolean remove(Palette p) {
+        String temp = colorsToString(p);
+        database.delete(DBManager.TABLE_PALETTES, DBManager.COLUMN_COLORS
+                + " = " + temp, null);
         return true; // Return true for success
     }
 
     /* While not strictly necessary, this could be useful for checking if we're trying to save
      * an identical palette / seeing if a palette has already been saved.
      */
-    public static boolean isSaved(Palette p) {
-        // Check if palette has already been inserted into favorites
-
-        return true; // Return true for success
+    public boolean isSaved(Palette p) {
+        String temp = colorsToString(p);
+        Cursor cursor = database.rawQuery("SELECT " + DBManager.COLUMN_COLORS
+                + " FROM " + DBManager.TABLE_PALETTES + " WHERE "
+                + DBManager.COLUMN_COLORS + " = " + temp, null);
+        if(cursor.getCount() > 0) return true; // Return true for success
+        return false;
 
     }
 
@@ -42,8 +80,37 @@ public class PaletteStorageHelper {
      * and I'll be giving a PaletteAdapter to FavoritePalettesActivity to populate
      * the ListView.
      */
-    public static Palette[] getAllPalettes() {
-        return null;
+    public Palette[] getAllPalettes() {
+        Cursor cursor = database.query(DBManager.TABLE_PALETTES,
+                allColumns, null, null, null, null, null);
+        Palette[] result = new Palette[cursor.getCount()];
+        cursor.moveToFirst();
+        int i = 0;
+        while (!cursor.isAfterLast()) {
+            Palette p = cursorToPalette(cursor);
+            result[i] = p;
+            i++;
+        }
+        return result;
+    }
+
+    private String colorsToString(Palette p) {
+        String ret = "";
+        for(int i = 0; i < p.getColors().length; i++) {
+            ret += Integer.toString(p.getColors()[i]);
+            if(i < p.getColors().length - 1) ret += ",";
+        }
+        return ret;
+    }
+
+    private Palette cursorToPalette(Cursor cursor) {
+        String[] colorTemp = cursor.getString(2).split(",");
+        int[] colors = new int[colorTemp.length];
+        for(int i = 0; i < colorTemp.length; i++) colors[i] = Integer.parseInt(colorTemp[i]);
+        String name = cursor.getString(1);
+        Palette p = new Palette(colors, null);
+        p.setName(name);
+        return p;
     }
 
     /* Again, this is just a rough outline for this interface. I've given my input as a sort
