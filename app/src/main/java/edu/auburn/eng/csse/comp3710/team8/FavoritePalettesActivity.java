8dfa3373219de203e1a32bdb3de24e8c6874e526
zealousduck <stewartpatrick12@gmail.com>
2015-04-26 19:18:06 -0500

    Began interfacing with Database, hooked up FavoritePalettesActivity

--- app/src/main/java/edu/auburn/eng/csse/comp3710/team8/FavoritePalettesActivity.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/team8/FavoritePalettesActivity.java
@@ -4,14 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.ListView;
 
 public class FavoritePalettesActivity extends Activity {
+
+    private PaletteStorageHelper psh;
+
+    private ListView mList;
+    private PaletteAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_palettes);
+
+        psh = new PaletteStorageHelper(FavoritePalettesActivity.this);
+
+        mList = (ListView)findViewById(R.id.list_favorited);
+        mAdapter = new PaletteAdapter(FavoritePalettesActivity.this, psh.getAllPalettes());
+        mList.setAdapter(mAdapter);
     }
 
 
